e68894ae4da3c033139dab41b01a160d19d25b1e
Hideaki Murakami <thehiderofkeys@gmail.com>
2021-12-20 21:21:10 +1300

    Finish day 4

---
+++ day4a.py
@@ -0,0 +1,38 @@
+def main():
+    draw = [int(call) for call in input().split(',')]
+    min_index = None
+    winner_score = None
+    try:
+        while True:
+            input()
+            score, index = bingo_board(draw)
+            if min_index is None or index < min_index:
+                min_index = index
+                winner_score = score
+    except:
+        last_number = draw[min_index]
+        print(winner_score)
+        print(last_number)
+        
+
+def bingo_board(draw):
+    board = [0] * len(draw)
+    last_index_rows = [0] * 5
+    last_index_cols = [0] * 5
+    for row_index in range(5):
+        row = [int(call) for call in input().split(' ') if call]
+        for col_index in range(5):
+            number = row[col_index]
+            index = draw.index(number)
+            board[index] = number
+            if last_index_rows[row_index] < index:
+                last_index_rows[row_index] = index
+            if last_index_cols[col_index] < index:
+                last_index_cols[col_index] = index
+    win_index = min(min(last_index_rows),min(last_index_cols))
+    score = sum(board) - sum(board[:win_index + 1])
+    return score, win_index
+
+
+if __name__ == "__main__":
+    main()
