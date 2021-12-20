8a5d59c2e2ef8a284df6b88ff7c2ee655bb4a635
Hideaki Murakami <thehiderofkeys@gmail.com>
2021-12-20 21:20:35 +1300

    Finish day 1

---
+++ day1a.py
@@ -0,0 +1,14 @@
+def main():
+    last = int(input())
+    sum = 0
+    try:
+        while(True):
+            curr = int(input())
+            if (curr > last):
+                sum += 1
+            last = curr
+    except ValueError:
+        print(sum)
+
+if __name__ == "__main__":
+    main()
---
+++ day1b.py
@@ -0,0 +1,21 @@
+def ingest() -> list[int]: 
+    output = []
+    try:
+        while(True):
+            output.append(int(input()))
+    except ValueError:
+        return output
+
+def main(input_array:list[int]):
+    last = sum(input_array[0:3])
+    total = 0
+    for i in range(1, len(input_array) - 2):
+        curr = sum(input_array[i:i+3])
+        if(curr > last):
+            total+=1
+        last = curr
+    print(total)
+
+if __name__ == "__main__":
+    input_values = ingest()
+    main(input_values)
